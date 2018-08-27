955d2821cfb1918ba002aa7820f2182112baefe9
Alan Ip <theipster@users.noreply.github.com>
2018-08-27 19:24:26 +0100

    Add bidirectional one to many mapping from Manufacturer to Model.

--- src/main/java/com/theipster/thegarage/data/Manufacturer.java
+++ src/main/java/com/theipster/thegarage/data/Manufacturer.java
@@ -1,9 +1,11 @@
 package com.theipster.thegarage.data;
 
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Manufacturer {
@@ -12,7 +14,14 @@ public class Manufacturer {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
+	@OneToMany(mappedBy = "manufacturer")
+	private List<Model> models;
+
 	private String name;
+
+	public List<Model> getModels() {
+		return this.models;
+	}
 
 	public String getName() {
 		return this.name;
